c93911f6fc8ce3bcaf8434ed67c4e0b3ee4ec3a3
Siddhartha Basu <biosidd@gmail.com>
2014-11-21 18:35:38 +0000

    Added few gulp tasks for managing bootstrap and flat-ui css files.

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,28 @@ var os = require('os');
 var del = require('del');
 var path = require('path');
 var fs = require('fs');
+var mkdirp = require('mkdirp');
+var request = require('request');
+var zlib = require('zlib');
+var tar = require('tar');
+
+
+var LATEST_BOOTSTRAP_URL = 'https://github.com/twbs/bootstrap/archive/v3.3.1.tar.gz';
+var LATEST_FLATUI_URL = 'https://github.com/designmodo/Flat-UI/archive/2.2.2.tar.gz';
+
+function getVendorFolder() {
+    return path.resolve(__dirname, 'src', 'styles', 'vendor');
+}
+
+function downloadAndExtract(url, path) {
+    request
+    .get(url)
+    .on('error',function(err) {
+        gutil.log(gutil.colors.red(err));
+    })
+    .pipe(zlib.createGunzip())
+    .pipe(tar.Extract({path: path}));
+}
 
 
 var ipAddr = os.networkInterfaces().eth0[0].address;
@@ -24,14 +46,14 @@ gulp.task("webpack-dev-server", function(callback) {
 });
 
 gulp.task("clean:vendor", function() {
-    var vendorPath = path.resolve(__dirname, 'src', 'styles', 'vendor');
+    var vendorPath = getVendorFolder();
     if (fs.lstatSync(vendorPath).isDirectory()) {
         del(vendorPath, function(err) {
             if (err) {
                 gutil.PluginError(err);
             }
             else {
-                gutil.log("deleted ", vendorPath);
+                gutil.log("deleted ", gutil.colors.cyan(vendorPath));
             }
         });
     }
@@ -39,6 +61,26 @@ gulp.task("clean:vendor", function() {
         gutil.log("path ", vendorPath, " does not exist !!!");
     }
 });
+
+gulp.task("create:vendor", function() {
+    var vendorPath = getVendorFolder();
+    mkdirp(vendorPath, function(err) {
+        if (err) {
+            gutil.log(gutil.colors.red(err));
+        }
+    });
+});
+
+gulp.task("download:bootstrap", function() {
+    downloadAndExtract(LATEST_BOOTSTRAP_URL, getVendorFolder());
+});
+
+
+gulp.task("download:flatui", function() {
+    downloadAndExtract(LATEST_FLATUI_URL, getVendorFolder());
+});
+
+gulp.task("setup:flatcss", ["clean:vendor", "create:vendor", "download:bootstrap", "download:flatui"]);
 
 // The development server (the recommended option for development)
 gulp.task("default", ["webpack-dev-server"]);
