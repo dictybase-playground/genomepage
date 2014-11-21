fffcafe156426f257e7fee0a5465fd18ca993d09
Siddhartha Basu <biosidd@gmail.com>
2014-11-21 16:21:58 +0000

    Added a cleanup task of vendor directory

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ var webpack = require("webpack");
 var WebpackDevServer = require("webpack-dev-server");
 var webpackConfig = require("./webpack.config.js");
 var os = require('os');
+var del = require('del');
+var path = require('path');
+var fs = require('fs');
 
 
 var ipAddr = os.networkInterfaces().eth0[0].address;
@@ -18,6 +21,23 @@ gulp.task("webpack-dev-server", function(callback) {
 		if(err) throw new gutil.PluginError("webpack-dev-server", err);
 		gutil.log("[webpack-dev-server]", "http://" + ipAddr + ":9000/webpack-dev-server/index.html");
 	});
+});
+
+gulp.task("clean:vendor", function() {
+    var vendorPath = path.resolve(__dirname, 'src', 'styles', 'vendor');
+    if (fs.lstatSync(vendorPath).isDirectory()) {
+        del(vendorPath, function(err) {
+            if (err) {
+                gutil.PluginError(err);
+            }
+            else {
+                gutil.log("deleted ", vendorPath);
+            }
+        });
+    }
+    else {
+        gutil.log("path ", vendorPath, " does not exist !!!");
+    }
 });
 
 // The development server (the recommended option for development)
