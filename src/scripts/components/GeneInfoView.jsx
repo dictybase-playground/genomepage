7ca13d92957f02dd4601054784d91be6cdbc02b4
Siddhartha Basu <biosidd@gmail.com>
2014-12-04 16:40:20 +0000

    Added fake data in two panels

--- src/scripts/components/GeneInfoView.jsx
+++ src/scripts/components/GeneInfoView.jsx
@@ -14,6 +14,30 @@ var GeneInfoView = React.createClass({
                                 DPU_G0051604
                             </div>
                         </div>
+                        <div className="row">
+                            <div className="col-xs-4">
+                                Gene ID
+                            </div>
+                            <div className="col-xs-8">
+                                DPU_G0051604
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-xs-4">
+                                Community Annotations
+                            </div>
+                            <div className="col-xs-8">
+                                <a href="http://wiki.dictybase.org/dictywiki/index.php/DPU_G0051064?action=edit">
+                                    Add an annotation for DPU_G0051604
+                                </a>
+                                &nbsp;
+                                &nbsp;
+                                &nbsp;
+                                <a href="http://wiki.dictybase.org/dictywiki/index.php/Community_Annotations">
+                                    Community Annotations Help
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </Panel>
         );
--- src/scripts/components/ProductView.jsx
+++ src/scripts/components/ProductView.jsx
@@ -5,6 +5,54 @@ var ProductView = React.createClass({
     render: function() {
         return (
             <Panel title="Gene Product Information" id="geneproduct" open={true}>
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col-xs-6">
+                                <div className="row">
+                                    <div className="col-xs-4">
+                                        Protein Coding Gene
+                                    </div>
+                                    <div className="col-xs-8">
+                                        DPU0051065
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-xs-4">
+                                        Protein  Length
+                                    </div>
+                                    <div className="col-xs-8">
+                                        70 aa
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-xs-4">
+                                        Protein Molecular Weight
+                                    </div>
+                                    <div className="col-xs-8">
+                                        7887.4 Da
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-xs-4">
+                                        More Protein Data
+                                    </div>
+                                    <div className="col-xs-8">
+                                        <a href="#">Protein sequence, domains and much more ....</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xs-6">
+                                <div className="row">
+                                    <div className="col-xs-4">
+                                        Genomic Coordinates
+                                    </div>
+                                    <div className="col-xs-8">
+                                        Table
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
             </Panel>
         );
     }
