1261d50a751fd0740b11cd783843d2d7787cab8a
Siddhartha Basu <biosidd@gmail.com>
2014-12-04 16:43:26 +0000

    Added fake content to links view

--- src/scripts/components/GeneLinksView.jsx
+++ src/scripts/components/GeneLinksView.jsx
@@ -5,6 +5,18 @@ var GeneLinksView = React.createClass({
     render: function() {
         return (
             <Panel title="Links" id="genelinks" open={true}>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-xs-4">
+                            External Resources
+                        </div>
+                        <div className="col-xs-8">
+                            <a href="http://genome.jgi-psf.org/cgi-bin/dispGeneModel?db=Dicpu1&id=146457">
+                                JGI_DPUR:146457
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </Panel>
         );
     }
