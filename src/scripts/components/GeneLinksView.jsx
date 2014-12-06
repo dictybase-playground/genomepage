cbf7f9f6370d9917b71f7972f4d66e43434c42e9
Siddhartha Basu <biosidd@gmail.com>
2014-12-06 15:26:11 +0000

    Added a separate method to handle the link generation

--- src/scripts/components/GeneLinksView.jsx
+++ src/scripts/components/GeneLinksView.jsx
@@ -1,23 +1,25 @@
 var React = require('react');
 var Panel = require('dicty-react-components').Panel;
 var ContainerClass = require('../utils/mixins').ContainerClass;
+var SplitRowView = require('./SplitRowView');
 
 var GeneLinksView = React.createClass({
     mixins: [ContainerClass],
+    getExternalLink: function () {
+        return (
+            <a href="http://genome.jgi-psf.org/cgi-bin/dispGeneModel?db=Dicpu1&id=146457">
+                JGI_DPUR:146457
+            </a>
+        )
+    },
     render: function() {
         return (
             <Panel title="Links" id="genelinks" open={true}>
                 <div className={this.getContainerClass()}>
-                    <div className="row">
-                        <div className="col-xs-4">
-                            External Resources
-                        </div>
-                        <div className="col-xs-8">
-                            <a href="http://genome.jgi-psf.org/cgi-bin/dispGeneModel?db=Dicpu1&id=146457">
-                                JGI_DPUR:146457
-                            </a>
-                        </div>
-                    </div>
+                    <SplitRowView
+                        leftColData="External Resource"
+                        rightColData={this.getExternalLink()}
+                    />
                 </div>
             </Panel>
         );
