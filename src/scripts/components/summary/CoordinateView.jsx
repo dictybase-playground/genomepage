eac9fbdd206feae111956e2afeba357fbb783b75
Siddhartha Basu <biosidd@gmail.com>
2014-12-10 21:02:25 +0000

    Added a table view for the coordinates

--- src/scripts/components/summary/CoordinateView.jsx
+++ src/scripts/components/summary/CoordinateView.jsx
@@ -2,6 +2,29 @@ var ContainerClass = require('../../utils/mixins').ContainerClass;
 var SplitRowView = require('../SplitRowView');
 var React = require('react');
 
+var TableView = React.createClass({
+    render: function() {
+        return (
+            <table className="table table-bordered">
+                <thead>
+                    <tr>
+                        <th> Exon </th>
+                        <th> Local coords. </th>
+                        <th> Chrom coords. </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td> 1 </td>
+                        <td> 1 - 213 </td>
+                        <td> 636 - 848 </td>
+                    </tr>
+                </tbody>
+            </table>
+        );
+    }
+})
+
 var CoordinateView = React.createClass({
     mixins: [ContainerClass],
     render: function() {
@@ -9,7 +32,7 @@ var CoordinateView = React.createClass({
             <div className={this.getContainerClass()}>
                 <SplitRowView
                     leftColData="Genomic Coordinates"
-                    rightColData="Table"
+                    rightColData={<TableView />}
                 />
             </div>
         );
