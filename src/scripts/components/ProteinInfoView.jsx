546d4539c26bd1152ceb182c70b4fb8f3de0a31a
Siddhartha Basu <biosidd@gmail.com>
2014-12-11 16:33:35 +0000

    Added content for Protein info component

--- src/scripts/components/ProteinInfoView.jsx
+++ src/scripts/components/ProteinInfoView.jsx
@@ -1,12 +1,32 @@
 var React = require('react');
 var Panel = require('dicty-react-components').Panel;
 var ContainerClass = require('../utils/mixins').ContainerClass;
+var SplitRowView = require('./SplitRowView');
+var AminoLinkView = require('./protein/AminoLinkView');
 
 var ProteinInfoView = React.createClass({
     mixins: [ContainerClass],
     render: function() {
         return (
             <Panel title="General Information" id="proteininfo" open={true}>
+                <div className={this.getContainerClass()}>
+                    <SplitRowView
+                        leftColData="Protein ID"
+                        rightColData="DPU1258995"
+                    />
+                    <SplitRowView
+                        leftColData="Protein Length"
+                        rightColData="70 aa"
+                    />
+                    <SplitRowView
+                        leftColData="Molecular Weight"
+                        rightColData="7887.4 Da"
+                    />
+                    <SplitRowView
+                        leftColData="AA Composition"
+                        rightColData={<AminoLinkView />}
+                    />
+                </div>
             </Panel>
         );
     }
