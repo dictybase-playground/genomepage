22b01dc8416adaf012bad2744598d698cd5d83fd
Siddhartha Basu <biosidd@gmail.com>
2014-12-11 16:32:59 +0000

    Added a component for creating link of amino acid component

---
+++ src/scripts/components/protein/AminoLinkView.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var AminoLinkView = React.createClass({
+    render: function() {
+        return (
+            <a href="#">View Amino Acid Composition</a>
+        );
+    }
+});
+
+module.exports = AminoLinkView;
