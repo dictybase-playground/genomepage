45501206d1d6139b6501cbd9e2f883fd522fbf7d
Siddhartha Basu <biosidd@gmail.com>
2014-12-11 21:51:23 +0000

    Added ProteinSequence component

--- src/scripts/components/ProteinSequenceView.jsx
+++ src/scripts/components/ProteinSequenceView.jsx
@@ -1,10 +1,28 @@
 var React = require('react');
 var Panel = require('dicty-react-components').Panel;
+var ContainerClass = require('../utils/mixins').ContainerClass;
+var SplitRowView = require('./SplitRowView');
 
 var ProteinSequenceView = React.createClass({
+    mixins: [ContainerClass],
+    getSequence: function () {
+        return (
+            <p>
+               &gt;DPU1258995 <br/>
+                MASLIILSASLFISLLLPISFKETILIIFFLMILFIHLSPYFHINNSGTSSSRLPRRTVP <br/>
+                ISPSINKKIN <br/>
+            </p>
+        );
+    },
     render: function() {
         return (
             <Panel title="Protein Sequence" id="proteinsequence" open={true}>
+                <div className={this.getContainerClass()}>
+                    <SplitRowView
+                        leftColData="Protein Sequence"
+                        rightColData={this.getSequence()}
+                    />
+                </div>
             </Panel>
         );
     }
