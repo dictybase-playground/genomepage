eb261ef3d08e34615463f5e3f87100637e4bf6f3
Siddhartha Basu <biosidd@gmail.com>
2015-09-18 21:51:42 +0000

    Added some basic jsdoc tags

--- src/scripts/reducers/index.js
+++ src/scripts/reducers/index.js
@@ -1,3 +1,10 @@
+/*
+ * @type {Object} initialState - The initial state for the reducer
+ * @param {string} initialState.id - The gene id
+ * @param {Object} initialState.response - The response object
+ * @param {Object} initialState.error - The error object
+ * @param {boolean} initialState.loading - Toggle the loading status
+*/
 const initialState = {
     id: null,
     response: null,
@@ -6,7 +13,13 @@ const initialState = {
 };
 
 
-export function geneIdReducer(state = initialState, action) {
+/**
+* @function geneIdReducer
+* @desc Reducer for GENEID action types
+* @param {Object} The initial state
+* @param {Object} The action object
+ */
+export const geneIdReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'GENEID_REQUEST':
             return {
@@ -35,8 +48,13 @@ export function geneIdReducer(state = initialState, action) {
     }
 }
 
-
-export function tabIdReducer(state = initialState, action) {
+/**
+* @function tabIdReducer
+* @desc Reducer for TABID action types
+* @param {Object} The initial state
+* @param {Object} The action object
+ */
+export const tabIdReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'TABID_REQUEST':
             return {
